a34c2b0af3aeb57a997a296f10ed2f0738dd2049
James McCarthy <james.mccarthy@uwaterloo.ca>
2015-12-03 09:42:04 -0500

    Add URL parsing that supports setting the left and right map URLs from URL parameters.

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ function updateCenterInputs(map){
     document.getElementById('center_long').value = lng;
 
 }
+
+/**
+  addTilesToURL
+  - map_index - 0 is left map, 1 is right map
+  - tileURL - URL of the tileset being added
+*/
+function addTilesToURL(map_index,tileURL){
+  var url = URI(window.location.href);
+
+  if (map_index == 0){
+    url.removeSearch("leftmap");
+    url.addSearch("leftmap",tileURL);
+    window.history.replaceState(null,null,url.href());
+  }
+  else if (map_index == 1){
+    url.removeSearch("rightmap");
+    url.addSearch("rightmap",tileURL);
+    window.history.replaceState(null,null,url.href());
+  }
+}
--- loader.js
+++ loader.js
@@ -17,16 +17,20 @@ function initMaps(){
       left_map.setView(e.target.getCenter(),e.target.getZoom());
       updateCenterInputs(e.target);
     });
+
+    loadTilesFromURL();
 }
 
 function loadLeftTiles(){
   var tile_url = document.getElementById('left-map-url').value;
   loadTiles(left_map,tile_url);
+  addTilesToURL(0,tile_url);
 }
 
 function loadRightTiles(){
   var tile_url = document.getElementById('right-map-url').value;
   loadTiles(right_map,tile_url);
+  addTilesToURL(1,tile_url);
 }
 
 function loadTiles(map,tile_URL){
@@ -34,4 +38,31 @@ function loadTiles(map,tile_URL){
   var long = document.getElementById('center_long').value;
   L.tileLayer(tile_URL).addTo(map);
   map.setView([lat,long]);
+}
+
+/**
+  loadTilesFromURL
+*/
+function loadTilesFromURL(){
+  var url = URI(window.location.href);
+  var left_url = url.query(true).leftmap;
+  var right_url = url.query(true).rightmap;
+
+  // remove trailing slash from URLs if they exist
+  if (left_url.substr(-1) === '/'){
+    var left_url = left_url.substr(0,left_url.length -1);
+  }
+  if (right_url.substr(-1) === '/'){
+    var right_url = right_url.substr(0,right_url.length -1);
+  }
+
+
+  if (left_url != undefined) {
+    loadTiles(left_map,left_url);
+  }
+  if (right_url != undefined) {
+    loadTiles(right_map,right_url);
+  }
+
+
 }
