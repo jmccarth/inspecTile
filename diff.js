f0764b6caa96cef7e68e4ec539254d4936365da6
James McCarthy <james.mccarthy@uwaterloo.ca>
2016-05-27 15:05:41 -0400

    Flagged tiles functionality added

--- diff.js
+++ diff.js
@@ -1,3 +1,5 @@
+var last_highlighted_id;
+
 function handleFileUpload(){
   var file = this.files[0];
   console.log(file);
@@ -22,8 +24,50 @@ function parseDiffFileContents(file_contents){
     new_li.appendChild(document.createTextNode(tile_id));
     diff_list_html.appendChild(new_li);
     new_li.addEventListener('click',zoomToTileFromID.bind(null,tile_id));
+    var new_flag = document.createElement("input");
+    new_flag.type = 'checkbox';
+    new_flag.id = 'chk_' + tile_id;
+    new_flag.addEventListener('click',flagCheckboxToggled.bind(new_flag,tile_id));
+    new_li.appendChild(new_flag);
+
 
   }
+  document.getElementById("num_diff_tiles").textContent = diffs.length
+}
+
+function flagCheckboxToggled(tile_id){
+  event.stopPropagation();
+  if(this.checked){
+    flagTile(tile_id);
+  }
+  else{
+    unflagTile(tile_id);
+  }
+}
+
+function flagTile(tile_id){
+  var flag_list_html = document.getElementById('flagged_tiles');
+  var num_flagged = document.getElementById('num_flagged_tiles').textContent;
+  var new_li = document.createElement("li");
+  new_li.id = 'flag_' + tile_id;
+  new_li.appendChild(document.createTextNode(tile_id));
+  new_li.addEventListener('click',unflagTile.bind(new_li, tile_id));
+  flag_list_html.appendChild(new_li);
+  document.getElementById('num_flagged_tiles').textContent = parseInt(num_flagged) + 1;
+
+  var diff_checkbox = document.getElementById('chk_' + tile_id);
+  diff_checkbox.checked = true;
+}
+
+function unflagTile(tile_id){
+  var flag_list_html = document.getElementById('flagged_tiles');
+  var num_flagged = document.getElementById('num_flagged_tiles').textContent;
+  var li = document.getElementById('flag_' + tile_id);
+  flag_list_html.removeChild(li);
+  document.getElementById('num_flagged_tiles').textContent = parseInt(num_flagged) - 1;
+
+  var diff_checkbox = document.getElementById('chk_' + tile_id);
+  diff_checkbox.checked = false;
 }
 
 function zoomToTileFromID(tile_id){
@@ -39,6 +83,7 @@ function zoomToTileFromID(tile_id){
 }
 
 function highlightTiles(tile_id){
+
   var matching_tiles = []
   var tiles = document.getElementsByTagName('img');
   for (tile in tiles){
@@ -46,14 +91,19 @@ function highlightTiles(tile_id){
       if (tiles[tile].src.includes(tile_id)){
         matching_tiles.push(tiles[tile]);
       }
+      else if (tiles[tile].src.includes(last_highlighted_id)){
+        tiles[tile].style.zIndex = "";
+        tiles[tile].style.boxShadow = "";
+      }
     }
   }
 
   for (tile in matching_tiles){
     matching_tiles[tile].style.zIndex = "5000";
     matching_tiles[tile].style.boxShadow = "0 0 10px black"
-
   }
+
+  last_highlighted_id = tile_id;
 }
 
 // copied from http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#X_and_Y
